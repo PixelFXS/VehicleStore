4e4bc00ea982f60d59f65ddb64c101108f62e04c
pixelsd <alexxag7jj@gmail.com>
2024-07-06 11:03:07 -0600

    Empty project, it's just going to start

---
+++ src/main/java/ApiService/VehiclesStore/VehiclesStoreApplication.java
@@ -0,0 +1,13 @@
+package ApiService.VehiclesStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiclesStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiclesStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ApiService/VehiclesStore/VehiclesStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ApiService.VehiclesStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiclesStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
